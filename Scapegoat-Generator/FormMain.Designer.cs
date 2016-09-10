abe7a6a01bc11cf5313aeee1be0febdf397587b9
Miau Shiauwe <mxw_19940830@sina.com>
2016-09-10 14:32:33 +0800

    Basic function completed

--- Scapegoat-Generator/FormMain.Designer.cs
+++ Scapegoat-Generator/FormMain.Designer.cs
@@ -34,8 +34,11 @@
             this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
             this.buttonGenerate = new System.Windows.Forms.Button();
             this.buttonSave = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.buttonCopyToClipboard = new System.Windows.Forms.Button();
             this.pictureBoxResult = new System.Windows.Forms.PictureBox();
+            this.textBoxContent = new System.Windows.Forms.TextBox();
+            this.labelContent = new System.Windows.Forms.Label();
+            this.labelHint = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxResult)).BeginInit();
             this.SuspendLayout();
@@ -52,11 +55,13 @@
             // 
             // textBoxQQ
             // 
+            this.textBoxQQ.CausesValidation = false;
             this.textBoxQQ.Location = new System.Drawing.Point(102, 12);
             this.textBoxQQ.MaxLength = 15;
             this.textBoxQQ.Name = "textBoxQQ";
             this.textBoxQQ.Size = new System.Drawing.Size(126, 21);
             this.textBoxQQ.TabIndex = 1;
+            this.textBoxQQ.TextChanged += new System.EventHandler(this.textBoxQQ_TextChanged);
             // 
             // errorProvider
             // 
@@ -66,30 +71,38 @@
             // buttonGenerate
             // 
             this.buttonGenerate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonGenerate.Location = new System.Drawing.Point(188, 39);
+            this.buttonGenerate.Enabled = false;
+            this.buttonGenerate.Location = new System.Drawing.Point(188, 71);
             this.buttonGenerate.Name = "buttonGenerate";
             this.buttonGenerate.Size = new System.Drawing.Size(68, 23);
-            this.buttonGenerate.TabIndex = 2;
+            this.buttonGenerate.TabIndex = 3;
             this.buttonGenerate.Text = " 走你！";
             this.buttonGenerate.UseVisualStyleBackColor = true;
+            this.buttonGenerate.Click += new System.EventHandler(this.buttonGenerate_Click);
             // 
             // buttonSave
             // 
-            this.buttonSave.Location = new System.Drawing.Point(175, 237);
+            this.buttonSave.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSave.Enabled = false;
+            this.buttonSave.Location = new System.Drawing.Point(175, 273);
             this.buttonSave.Name = "buttonSave";
             this.buttonSave.Size = new System.Drawing.Size(75, 23);
-            this.buttonSave.TabIndex = 4;
+            this.buttonSave.TabIndex = 5;
             this.buttonSave.Text = "保存到本地";
             this.buttonSave.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // buttonCopyToClipboard
             // 
-            this.button1.Location = new System.Drawing.Point(68, 237);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(90, 23);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "复制到剪贴板";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonCopyToClipboard.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCopyToClipboard.Location = new System.Drawing.Point(68, 273);
+            this.buttonCopyToClipboard.Name = "buttonCopyToClipboard";
+            this.buttonCopyToClipboard.Size = new System.Drawing.Size(90, 23);
+            this.buttonCopyToClipboard.TabIndex = 4;
+            this.buttonCopyToClipboard.Text = "复制到剪贴板";
+            this.buttonCopyToClipboard.UseVisualStyleBackColor = true;
+            this.buttonCopyToClipboard.Click += new System.EventHandler(this.buttonCopyToClipboard_Click);
             // 
             // pictureBoxResult
             // 
@@ -97,18 +110,49 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBoxResult.Image = global::Scapegoat_Generator.Properties.Resources.background;
             this.pictureBoxResult.InitialImage = null;
-            this.pictureBoxResult.Location = new System.Drawing.Point(12, 68);
+            this.pictureBoxResult.Location = new System.Drawing.Point(12, 105);
             this.pictureBoxResult.Name = "pictureBoxResult";
             this.pictureBoxResult.Size = new System.Drawing.Size(244, 155);
             this.pictureBoxResult.TabIndex = 3;
             this.pictureBoxResult.TabStop = false;
             // 
+            // textBoxContent
+            // 
+            this.textBoxContent.CausesValidation = false;
+            this.textBoxContent.Location = new System.Drawing.Point(102, 39);
+            this.textBoxContent.MaxLength = 15;
+            this.textBoxContent.Name = "textBoxContent";
+            this.textBoxContent.Size = new System.Drawing.Size(126, 21);
+            this.textBoxContent.TabIndex = 2;
+            // 
+            // labelContent
+            // 
+            this.labelContent.AutoSize = true;
+            this.labelContent.Location = new System.Drawing.Point(24, 42);
+            this.labelContent.Name = "labelContent";
+            this.labelContent.Size = new System.Drawing.Size(77, 12);
+            this.labelContent.TabIndex = 6;
+            this.labelContent.Text = "背锅的内容：";
+            this.labelContent.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // labelHint
+            // 
+            this.labelHint.AutoSize = true;
+            this.labelHint.Location = new System.Drawing.Point(12, 76);
+            this.labelHint.Name = "labelHint";
+            this.labelHint.Size = new System.Drawing.Size(0, 12);
+            this.labelHint.TabIndex = 0;
+            this.labelHint.Visible = false;
+            // 
             // formMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(268, 272);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(268, 309);
+            this.Controls.Add(this.labelHint);
+            this.Controls.Add(this.textBoxContent);
+            this.Controls.Add(this.labelContent);
+            this.Controls.Add(this.buttonCopyToClipboard);
             this.Controls.Add(this.buttonSave);
             this.Controls.Add(this.pictureBoxResult);
             this.Controls.Add(this.buttonGenerate);
@@ -132,8 +176,11 @@
         private System.Windows.Forms.ErrorProvider errorProvider;
         private System.Windows.Forms.Button buttonGenerate;
         private System.Windows.Forms.PictureBox pictureBoxResult;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonCopyToClipboard;
         private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.TextBox textBoxContent;
+        private System.Windows.Forms.Label labelContent;
+        private System.Windows.Forms.Label labelHint;
     }
 }
 
---
+++ Scapegoat-Generator/FormMain.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+
+namespace Scapegoat_Generator
+{
+    public partial class formMain : Form
+    {
+        public formMain()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxQQ_TextChanged(object sender,EventArgs e)
+        {
+            if(!textBoxQQ.Text.Equals(""))
+            {
+                long temp;
+                if(!long.TryParse(textBoxQQ.Text,out temp))
+                {
+                    errorProvider.SetError(textBoxQQ,"QQ号不正确");
+                    buttonGenerate.Enabled = false;
+                }
+                else
+                {
+                    errorProvider.Clear();
+                    buttonGenerate.Enabled = true;
+                }
+            }
+            else
+            {
+                errorProvider.Clear();
+                buttonGenerate.Enabled = false;
+            }
+        }
+
+        private async void buttonGenerate_Click(object sender,EventArgs e)
+        {
+            labelHint.Text = "背锅中……";
+            labelHint.Visible = true;
+
+            long QQNumber = long.Parse(textBoxQQ.Text);
+
+            pictureBoxResult.Image = Properties.Resources.background.Clone() as Bitmap;
+            Graphics newGraphic = Graphics.FromImage(pictureBoxResult.Image);
+
+            long currentTimestamp = DateTime.UtcNow.ToLocalTime().Ticks - new DateTime(1970,1,1,0,0,0).Ticks;
+            String url = "http://q2.qlogo.cn/headimg_dl?bs=qq&dst_uin=" + QQNumber.ToString() + "&spec=100&times=" + currentTimestamp / 1000;
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+            request.Timeout = 20000;
+            HttpWebResponse response = await request.GetResponseAsync() as HttpWebResponse;
+            Stream headerStream = response.GetResponseStream();
+
+            Brush backgroundBrush = new SolidBrush(Color.White);
+            newGraphic.FillRectangle(backgroundBrush,new Rectangle(0,120,244,155));
+            Brush textBrush = new SolidBrush(Color.Black);
+            newGraphic.DrawString(textBoxContent.Text,new Font("微软雅黑",20),textBrush,0,120);
+            newGraphic.DrawImage(Image.FromStream(headerStream),141,0,100,100);
+            newGraphic.Dispose();
+            pictureBoxResult.Refresh();
+
+            labelHint.Text = "背锅成功！";
+        }
+
+        private void buttonCopyToClipboard_Click(object sender,EventArgs e)
+        {
+            Bitmap bitmapToClipboard = new Bitmap(pictureBoxResult.Width,pictureBoxResult.Height);
+            pictureBoxResult.DrawToBitmap(bitmapToClipboard,pictureBoxResult.ClientRectangle);
+            System.Windows.Forms.Clipboard.SetImage(bitmapToClipboard);
+        }
+    }
+}
