2a74fca7ffb1b0929479c0b7f68e4f2de0250391
Miau Shiauwe <mxw_19940830@sina.com>
2016-09-10 12:11:58 +0800

    GUI determined

--- Scapegoat-Generator/Form1.Designer.cs
+++ Scapegoat-Generator/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Scapegoat_Generator
 {
-    partial class Form1
+    partial class formMain
     {
         /// <summary>
         /// 必需的设计器变量。
@@ -29,11 +29,111 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.labelScapegoat = new System.Windows.Forms.Label();
+            this.textBoxQQ = new System.Windows.Forms.TextBox();
+            this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBoxResult = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelScapegoat
+            // 
+            this.labelScapegoat.AutoSize = true;
+            this.labelScapegoat.Location = new System.Drawing.Point(12, 16);
+            this.labelScapegoat.Name = "labelScapegoat";
+            this.labelScapegoat.Size = new System.Drawing.Size(89, 12);
+            this.labelScapegoat.TabIndex = 0;
+            this.labelScapegoat.Text = "背锅人的QQ号：";
+            this.labelScapegoat.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // textBoxQQ
+            // 
+            this.textBoxQQ.Location = new System.Drawing.Point(102, 12);
+            this.textBoxQQ.MaxLength = 15;
+            this.textBoxQQ.Name = "textBoxQQ";
+            this.textBoxQQ.Size = new System.Drawing.Size(126, 21);
+            this.textBoxQQ.TabIndex = 1;
+            // 
+            // errorProvider
+            // 
+            this.errorProvider.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.NeverBlink;
+            this.errorProvider.ContainerControl = this;
+            // 
+            // buttonGenerate
+            // 
+            this.buttonGenerate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonGenerate.Location = new System.Drawing.Point(188, 39);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(68, 23);
+            this.buttonGenerate.TabIndex = 2;
+            this.buttonGenerate.Text = " 走你！";
+            this.buttonGenerate.UseVisualStyleBackColor = true;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(175, 237);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 4;
+            this.buttonSave.Text = "保存到本地";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(68, 237);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "复制到剪贴板";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBoxResult
+            // 
+            this.pictureBoxResult.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxResult.Image = global::Scapegoat_Generator.Properties.Resources.background;
+            this.pictureBoxResult.InitialImage = null;
+            this.pictureBoxResult.Location = new System.Drawing.Point(12, 68);
+            this.pictureBoxResult.Name = "pictureBoxResult";
+            this.pictureBoxResult.Size = new System.Drawing.Size(244, 155);
+            this.pictureBoxResult.TabIndex = 3;
+            this.pictureBoxResult.TabStop = false;
+            // 
+            // formMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(268, 272);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.pictureBoxResult);
+            this.Controls.Add(this.buttonGenerate);
+            this.Controls.Add(this.textBoxQQ);
+            this.Controls.Add(this.labelScapegoat);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "formMain";
+            this.Text = "背锅生成器";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxResult)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelScapegoat;
+        private System.Windows.Forms.TextBox textBoxQQ;
+        private System.Windows.Forms.ErrorProvider errorProvider;
+        private System.Windows.Forms.Button buttonGenerate;
+        private System.Windows.Forms.PictureBox pictureBoxResult;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonSave;
     }
 }
 
